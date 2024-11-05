ac24a4295a708339d6093a0ff427130c03e889d9
SimaSava <simasavar@gmail.com>
2024-11-05 15:43:57 -0500

    please check my assignment

---
+++ Attempt-1.py
@@ -0,0 +1,5 @@
+name=input("enter your name:")
+if name.capitalize()=="Sima":
+      print("Hello,{}! The password is :{}".format("Sima","w@12"))
+else:
+      print("Hello",name.capitalize(),"! See you later.")
